a86e8b0316519620e81ea5619b36354d2a1e631b
Muriel Silveira <murielkong@gmail.com>
2016-07-08 01:58:37 -0300

    Improve logic

--- matrix.go
+++ matrix.go
@@ -3,14 +3,8 @@ package gomuriel
 func SumDiagonals(matrix [][]int, size int) int {
   var sum = 0
   for i := 0; i < size; i = i + 1 {
-    for j := 0; j < size; j = j + 1 {
-      if i == j {
-        sum = sum + matrix[i][j]
-      }
-      if i == (size - j - 1) {
-        sum = sum - matrix[i][j]
-      }
-    }
+    sum = sum + matrix[i][i]
+    sum = sum - matrix[i][size - i - 1]
   }
   if sum < 0 {
     sum = sum * -1
