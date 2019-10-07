5b291b9b8d62e6909592caa87e0bc65154c6a11f
Muriel Silveira <murielkong@gmail.com>
2019-10-07 18:04:33 +0200

    chore: go fmt .

--- matrix.go
+++ matrix.go
@@ -1,13 +1,13 @@
 package gomuriel
 
 func SumDiagonals(matrix [][]int, size int) int {
-  var sum = 0
-  for i := 0; i < size; i = i + 1 {
-    sum = sum + matrix[i][i]
-    sum = sum - matrix[i][size - i - 1]
-  }
-  if sum < 0 {
-    sum = sum * -1
-  }
-  return sum
+	var sum = 0
+	for i := 0; i < size; i = i + 1 {
+		sum = sum + matrix[i][i]
+		sum = sum - matrix[i][size-i-1]
+	}
+	if sum < 0 {
+		sum = sum * -1
+	}
+	return sum
 }
